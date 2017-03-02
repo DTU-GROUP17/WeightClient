75ac415133762fb7c4459615f16565dd85a53176
dukocuk <duran.kse@gmail.com>
2017-03-02 20:52:05 +0100

    coded some more ideas

--- WeightClient/src/Client.java
+++ WeightClient/src/Client.java
@@ -1,51 +1,75 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
 
-	Socket client;
-	int port;
-	DataInputStream input;
-	DataOutputStream output;
-	
-	//constructor for default port.
-	public Client() {
+	Socket sock = null;
+	String hostname;
+	int port = 8000; // default port
+	DataInputStream input = null;
+	DataOutputStream output = null;
+	BufferedReader br = null;
+
+	// constructor
+	public Client(String hostname, int port) {
 		try {
-			client = new Socket("localhost", port);
-			input = new DataInputStream(client.getInputStream());
-			output = new DataOutputStream(client.getOutputStream());
+			this.hostname = hostname;
+			this.port = port;
+			sock = new Socket(hostname, port);
+			// input = new DataInputStream(sock.getInputStream()); // maybe not
+			// needed since br maybe does the same
+			output = new DataOutputStream(sock.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
+			System.err.println("Don't know about host: " + hostname + "on port: " + port);
 			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the host: " + hostname + "on port: " + port);
+			e.printStackTrace();
+		}
+
+	}
+
+	public void send(String msg) {
+		if (sock != null && input != null && output != null) {
+			try {
+				output.writeBytes(msg);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// prefered way to send is trough this method, since there is a catch in it
+	public void receive() {
+		String responseLine;
+		try {
+			while ((responseLine = br.readLine()) != null) {
+				System.out.println("Server: " + responseLine);
+			}
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the host: " + hostname + "on port: " + port);
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+
+		// close down properly
+		try {
+			output.close();
+			input.close();
+			sock.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	//constructor if connecting to another port
-	public Client(int port) {
-		try {
-			client = new Socket("localhost", port);
-			input = new DataInputStream(client.getInputStream());
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- WeightClient/src/Main.java
+++ WeightClient/src/Main.java
@@ -6,9 +6,15 @@ public class Main {
 	Client client;
 	
 	public static void main(String[] args) {
-		Client client = new Client(Integer.parseInt(args[1]));
+		if (args.length < 1 | args.length < 2 | args.length > 3) {
+			System.out.println("usage Main <hostname> <port>");
+			System.exit(0);
+		}
+		Client client = new Client(args[0], Integer.parseInt(args[1]));
 		try {
 			client.output.writeBytes("test\n");
+			client.send("Hello");
+			client.receive();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -18,15 +24,7 @@ public class Main {
 		
 		
 		
-		//close down properly
-		try {
-			client.output.close();
-			client.input.close();
-			client.client.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
 		
 		
 		
