9b9042ea7cc80ad75eece331272fa2021ab2d5b7
dukocuk <duran.kse@gmail.com>
2017-03-14 06:55:42 +0100

    catching the exceptions at main

--- WeightClient/src/Main.java
+++ WeightClient/src/Main.java
@@ -17,13 +17,13 @@ public class Main {
 		System.exit(0);
 	}
 
-	public static void main(String[] args) throws IOException, ExceptionCommand {
+	public static void main(String[] args) throws IOException {
 		final Client client;
 		final String hostname;
 		final Scanner stdIn;
 		final Menu menu;
 		final Command command;
-		double tara, netto, brutto; 
+		double tara = 0, netto = 0, brutto = 0; 
 
 		if (args.length < 2 | args.length > 2) {
 			throw new IllegalArgumentException("\nusage: Main <hostname> <port>");
@@ -45,38 +45,94 @@ public class Main {
 			switch (input) {
 			case 1:
 					client.send("RM20 8 \"Indtast operatoernummer\" \"\" \"&3\"\r\n");
+				try {
 					command.RM20(client);
 					command.RM20(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("RM20 8 \"Indtast batchnummer\" \"\" \"&3\"\r\n");
+				try {
 					command.RM20(client);
 					command.RM20(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("P111 \"Vaegten skal vaere ubelastet\"\r\n");
+				try {
 					command.P111(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("T\r\n");
+				try {
 					command.T(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("RM20 8 \"Placer tom tara\" \"\" \"&3\"\r\n");
+				try {
 					command.RM20(client);
 					command.RM20(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("S\r\n");		
+				try {
 					tara = command.S(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("T\r\n");
+				try {
 					command.T(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("RM20 8 \"Placer netto\" \"\" \"&3\"\r\n");
+				try {
 					command.RM20(client);
 					command.RM20(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("S\r\n");		
+				try {
 					netto = command.S(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("T\r\n");
+				try {
 					command.T(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("RM20 8 \"Fjern brutto\" \"\" \"&3\"\r\n");
+				try {
 					command.RM20(client);
 					command.RM20(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("S\r\n");		 
+				try {
 					brutto = command.S(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("T\r\n");
+				try {
 					command.T(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 					client.send("P111 \"OK\"\r\n");
+				try {
 					command.P111(client);
+				} catch (ExceptionCommand e) {
+					e.getMessage();
+				}
 										
 					
 					menu.resultat(tara, netto, brutto);
